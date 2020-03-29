5f80f55bff178200465ed4ccb428549ac55fc06c
sidhub1 <ssr4@illinois.edu>
2020-03-29 12:30:18 -0400

    backend utils

---
+++ app/src/main/java/com/example/homie/config/Config.java
@@ -0,0 +1,6 @@
+package com.example.homie.config;
+
+public class Config {
+    public static final String PAYPAL_CLIENT_ID = "Aav8EaDxymTUfkcJ9y_qaTRhppQl5HKmelR5szzhCTFOkas3mniAFAJwmAJigM0vwuyaPp1PXg4NmF3R"
+;
+}
