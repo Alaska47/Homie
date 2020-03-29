acfd86662b79b170bce20295415a58c2ce128b15
sidhub1 <sid.rampally@gmail.com>
2020-03-28 23:18:28 -0400

    aneesh is a bot

--- app/src/main/java/com/example/homie/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/homie/fragments/ProfileFragment.java
@@ -1,13 +1,16 @@
 package com.example.homie.fragments;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.homie.R;
+import com.example.homie.activities.NewUpdateActivity;
 
 import androidx.fragment.app.Fragment;
 
@@ -20,6 +23,7 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button updateButton;
 
     private OnFragmentInteractionListener mListener;
 
@@ -52,13 +56,26 @@ public class ProfileFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_profile, container, false);
+        updateButton = v.findViewById(R.id.updateButton);
+
+        updateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), NewUpdateActivity.class);
+                startActivity(intent);
+            }
+        });
+        return v;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/com/example/homie/utils/RVAdapterDonation.java
+++ app/src/main/java/com/example/homie/utils/RVAdapterDonation.java
@@ -1,90 +1,93 @@
-package com.example.homie.utils;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-
-import com.example.homie.R;
-
-import java.util.List;
-
-import androidx.cardview.widget.CardView;
-import androidx.recyclerview.widget.RecyclerView;
-
-public class RVAdapterDonation extends RecyclerView.Adapter<RVAdapterDonation.DonationViewHolder>{
-    public static class DonationViewHolder extends RecyclerView.ViewHolder {
-        CardView cv;
-        ImageView backgroundImage;
-        TextView name;
-        TextView description;
-        ProgressBar moneyRaisedProgress;
-        TextView moneyRaisedDescription;
-        TextView numLikes;
-        TextView numKarma;
-
-        DonationViewHolder(View itemView) {
-            super(itemView);
-            cv = (CardView) itemView.findViewById(R.id.cv);
-            backgroundImage = (ImageView) itemView.findViewById(R.id.cv_background_image);
-            name = (TextView) itemView.findViewById(R.id.cv_name);
-            description = (TextView) itemView.findViewById(R.id.cv_description);
-            moneyRaisedProgress = (ProgressBar) itemView.findViewById(R.id.cv_raised_money);
-            moneyRaisedDescription = (TextView) itemView.findViewById(R.id.cv_raised_money_description);
-            numLikes = (TextView) itemView.findViewById(R.id.cv_num_likes);
-            numKarma = (TextView) itemView.findViewById(R.id.cv_num_karma);
-        }
-    }
-
-    List<StoryCard> storyCards;
-
-    public RVAdapterDonation(List<StoryCard> stories){
-        this.storyCards = stories;
-    }
-
-    @Override
-    public int getItemCount() {
-        if(storyCards == null)
-            return 0;
-        return storyCards.size();
-    }
-
-    public void clear() {
-        storyCards.clear();
-        notifyDataSetChanged();
-    }
-
-    @Override
-    public DonationViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.donation_row, viewGroup, false);
-        DonationViewHolder pvh = new DonationViewHolder(v);
-        return pvh;
-    }
-
-    @Override
-    public void onBindViewHolder(DonationViewHolder donationViewHolder, int i) {
-        storyViewHolder.backgroundImage.setImageBitmap(storyCards.get(i).backgroundImage);
-        storyViewHolder.name.setText(String.format("Help out %s", storyCards.get(i).name));
-        storyViewHolder.description.setText(storyCards.get(i).description);
-        storyViewHolder.moneyRaisedDescription.setText(String.format("$%,d raised of $%,d goal", storyCards.get(i).moneyRaised, storyCards.get(i).moneyRaisedGoal));
-        storyViewHolder.moneyRaisedProgress.setProgress((int) (storyCards.get(i).moneyRaised * 100.0 / (storyCards.get(i).moneyRaisedGoal)));
-        storyViewHolder.numLikes.setText(String.format("%s", format(storyCards.get(i).numLikes)));
-        storyViewHolder.numKarma.setText(String.format("%s", format(storyCards.get(i).numKarma)));
-    }
-
-    private String format(int num) {
-        if (num >= 100000)
-            return format(num / 1000) + "k";
-        if (num >= 1000) {
-            return String.format("%.1fk", num / 1000D);
-        }
-        return String.format("%,d", num);
-    }
-
-    @Override
-    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
-        super.onAttachedToRecyclerView(recyclerView);
-    }
-}
+//package com.example.homie.utils;
+//
+//import android.app.Activity;
+//import android.media.Image;
+//import android.util.Log;
+//import android.view.LayoutInflater;
+//import android.view.View;
+//import android.view.ViewGroup;
+//import android.widget.ImageView;
+//import android.widget.ProgressBar;
+//import android.widget.TextView;
+//
+//import com.example.homie.R;
+//
+//import java.util.List;
+//
+//import androidx.cardview.widget.CardView;
+//import androidx.recyclerview.widget.RecyclerView;
+//
+//public class RVAdapterStory extends RecyclerView.Adapter<RVAdapterStory.StoryViewHolder>{
+//    public static class StoryViewHolder extends RecyclerView.ViewHolder {
+//        CardView cv;
+//        ImageView backgroundImage;
+//        TextView name;
+//        TextView description;
+//        ProgressBar moneyRaisedProgress;
+//        TextView moneyRaisedDescription;
+//        TextView numLikes;
+//        TextView numKarma;
+//
+//        StoryViewHolder(View itemView) {
+//            super(itemView);
+//            cv = (CardView) itemView.findViewById(R.id.cv);
+//            backgroundImage = (ImageView) itemView.findViewById(R.id.cv_background_image);
+//            name = (TextView) itemView.findViewById(R.id.cv_name);
+//            description = (TextView) itemView.findViewById(R.id.cv_description);
+//            moneyRaisedProgress = (ProgressBar) itemView.findViewById(R.id.cv_raised_money);
+//            moneyRaisedDescription = (TextView) itemView.findViewById(R.id.cv_raised_money_description);
+//            numLikes = (TextView) itemView.findViewById(R.id.cv_num_likes);
+//            numKarma = (TextView) itemView.findViewById(R.id.cv_num_karma);
+//        }
+//    }
+//
+//    List<StoryCard> storyCards;
+//
+//    public RVAdapterStory(List<StoryCard> stories){
+//        this.storyCards = stories;
+//    }
+//
+//    @Override
+//    public int getItemCount() {
+//        if(storyCards == null)
+//            return 0;
+//        return storyCards.size();
+//    }
+//
+//    public void clear() {
+//        storyCards.clear();
+//        notifyDataSetChanged();
+//    }
+//
+//    @Override
+//    public StoryViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+//        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.story_card, viewGroup, false);
+//        StoryViewHolder pvh = new StoryViewHolder(v);
+//        return pvh;
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(StoryViewHolder storyViewHolder, int i) {
+//        storyViewHolder.backgroundImage.setImageBitmap(storyCards.get(i).backgroundImage);
+//        storyViewHolder.name.setText(String.format("Help out %s", storyCards.get(i).name));
+//        storyViewHolder.description.setText(storyCards.get(i).description);
+//        storyViewHolder.moneyRaisedDescription.setText(String.format("$%,d raised of $%,d goal", storyCards.get(i).moneyRaised, storyCards.get(i).moneyRaisedGoal));
+//        storyViewHolder.moneyRaisedProgress.setProgress((int) (storyCards.get(i).moneyRaised * 100.0 / (storyCards.get(i).moneyRaisedGoal)));
+//        storyViewHolder.numLikes.setText(String.format("%s", format(storyCards.get(i).numLikes)));
+//        storyViewHolder.numKarma.setText(String.format("%s", format(storyCards.get(i).numKarma)));
+//    }
+//
+//    private String format(int num) {
+//        if (num >= 100000)
+//            return format(num / 1000) + "k";
+//        if (num >= 1000) {
+//            return String.format("%.1fk", num / 1000D);
+//        }
+//        return String.format("%,d", num);
+//    }
+//
+//    @Override
+//    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+//        super.onAttachedToRecyclerView(recyclerView);
+//    }
+//}
